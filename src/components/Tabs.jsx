0dcdb6370073d2200129708c23796cf3476d3378
MikiZenebe <mikizenebe10gmail.com>
2024-10-29 23:39:42 +0300

    updated

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,39 +1,68 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Tabs = () => {
+  const [toggleState, setToggleState] = useState(1);
+  const [tabContent, setTabContent] = useState({});
 
-    // const tabs = [
-    //     {
-    //         id: 1,
-    //         tabTitle: 'Tab 1',
-    //         title: 'Title 1',
-    //         content: 'In sint do non adipisicing incididunt excepteur sit. Voluptate esse aliqua pariatur dolor ex occaecat sunt eu. Pariatur ullamco id dolore sint proident sint nostrud nisi sit id est. Duis et excepteur cupidatat sint nisi dolore qui irure qui in id excepteur irure laboris. Pariatur mollit duis cupidatat nisi Lorem non et in dolor aliquip ea sint aute. Dolore aute duis laboris exercitation occaecat sunt. Enim veniam Lorem do ipsum aliqua qui eu ipsum consectetur ex dolore ea ipsum.'
-    //     },
-    //     {
-    //         id: 2,
-    //         tabTitle: 'Tab 2',
-    //         title: 'Title 2',
-    //         content: 'Non aliquip fugiat velit ad officia Lorem tempor cillum incididunt elit proident mollit. Reprehenderit qui nisi ut occaecat minim velit deserunt occaecat quis magna mollit. Veniam proident consectetur sunt mollit est magna Lorem voluptate enim cupidatat consequat. Et pariatur aliquip commodo nisi deserunt exercitation enim officia voluptate in nisi. Eu ea esse qui est ea pariatur nostrud non elit irure. Ad exercitation Lorem exercitation ipsum eiusmod ea duis ad mollit veniam aliquip veniam. Lorem pariatur elit ea duis.'
-    //     },
-    //     {
-    //         id: 3,
-    //         tabTitle: 'Tab 3',
-    //         title: 'Title 3',
-    //         content: 'Deserunt et elit elit ad dolor magna. Nisi amet consectetur Lorem eiusmod dolore adipisicing do reprehenderit. Voluptate consequat magna nostrud in officia labore. Minim excepteur consectetur quis nostrud nisi magna duis sunt sint qui. Fugiat ea reprehenderit eiusmod proident officia. Consequat labore qui velit Lorem consectetur incididunt ut nisi.'
-    //     },
-    //     {
-    //         id: 4,
-    //         tabTitle: 'Tab 4',
-    //         title: 'Title 4',
-    //         content: 'Minim in dolor do fugiat laborum duis labore consectetur. Amet ut sint ipsum dolor ad nostrud commodo sunt veniam enim aliquip nulla sint ullamco. Do cupidatat et quis laborum esse est commodo. Commodo sunt consectetur do consequat minim occaecat id magna ullamco consequat irure.'
-    //     }
-    // ];
+  const toggleTab = (index) => {
+    setToggleState(index);
+    fetchTabContent(index);
+  };
 
-    return (
-        <div className='container'>
-            {/* TODO Add tabs here */}
-        </div>
-    );
-}
+  const fetchTabContent = async (index) => {
+    if (tabContent[index]) return; // Use cached data if available
+
+    try {
+      const lengths = ["short", "medium", "long", "verylong"];
+      const response = await axios.get(
+        `/api/1/${lengths[index - 1]}/plaintext`
+      );
+
+      const content = await response.text();
+
+      setTabContent((prevContent) => ({
+        ...prevContent,
+        [index]: content,
+      }));
+    } catch (error) {
+      console.error("Failed to fetch tab content:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTabContent(toggleState);
+  }, [toggleState]);
+
+  return (
+    <div className="container">
+      <div className="bloc-tabs">
+        {[1, 2, 3, 4].map((i) => (
+          <button
+            key={i}
+            className={toggleState === i ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(i)}
+          >
+            Tab {i}
+          </button>
+        ))}
+      </div>
+
+      <div className="content-tabs">
+        {[1, 2, 3, 4].map((i) => (
+          <div
+            key={i}
+            className={toggleState === i ? "content active-content" : "content"}
+          >
+            <h2>Title {i}</h2>
+            <p>{tabContent[i] || "Loading content..."}</p>
+
+            {tabContent && <p>{tabContent[i] || "Loading content..."}</p>}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Tabs;
